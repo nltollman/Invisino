08d7e93cabf06d80d34250169879772814e5804f
Maggie <msgembala@crimson.ua.edu>
2017-10-05 18:31:27 -0500

    Edited website

---
+++ app/Invisino/app/src/main/java/com/invisino/invisino/User.java
@@ -0,0 +1,16 @@
+package com.invisino.invisino;
+
+/**
+ * Created by Maggie Gembala: 10-5-2017
+ */
+
+public class User {
+
+    private int Uid;
+    private String email;
+    private String password;
+
+    User() {
+
+    }
+}
