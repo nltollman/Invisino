b29950151d3a385ba6867ebb5255897ed2efbabc
jrmelton50 <jrmelton@crimson.ua.edu>
2017-10-04 21:33:12 -0500

    Created My Exisiting Notes Activity Page without functionality

---
+++ app/Invisino/app/src/main/java/com/invisino/invisino/ViewMyExisitingNotes.java
@@ -0,0 +1,13 @@
+package com.invisino.invisino;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewMyExisitingNotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_my_exisiting_notes);
+    }
+}
